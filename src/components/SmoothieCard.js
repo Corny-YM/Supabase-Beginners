e00ecfe97836d1463175b65e3436462e187a6e3e
theanh1508 <85997901+0862551396@users.noreply.github.com>
2023-11-30 22:07:22 +0700

    remove navigate

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,8 +1,7 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import supabase from "../config/supabaseClient";
 
 const SmoothieCard = ({ smoothie, onDelete }) => {
-  const navigate = useNavigate();
 
   const handleDelete = async () => {
     const { error } = await supabase
